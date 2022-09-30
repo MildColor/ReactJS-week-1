f3ccfad083dd22ab5898683d74743480a01152e8
MildColor <96479626+MildColor@users.noreply.github.com>
2022-09-30 14:04:58 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+// import [패키지명] from [경로] 이 형식으로 불러와요.
+import React from "react";
+// js 파일 뿐 아니라 이미지도 가능가능!
+import logo from "./logo.svg";
+// css? 가능!
+import "./App.css";
 
 function App() {
   return (
