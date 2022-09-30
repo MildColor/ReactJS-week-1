338a34010dabbfcf08eed914e4a72597c9b5ba68
MildColor <96479626+MildColor@users.noreply.github.com>
2022-09-30 14:09:01 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 // import [패키지명] from [경로] 이 형식으로 불러와요.
 import React from "react";
-// js 파일 뿐 아니라 이미지도 가능가능!
-import logo from "./logo.svg";
-// css? 가능!
-import "./App.css";
-
+const cat_name = "perl";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // JSX에서 js값을 가져오려면 중괄호를 쓴다.
+  return <div>hello {cat_name}!</div>;
 }
 
 export default App;
